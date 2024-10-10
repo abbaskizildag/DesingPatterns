7070a4ae851006909b5f36de2dd140fe871cadfd
Ali Kızıldağ <abbaskizildag@gmail.com>
2024-10-10 16:52:38 +0300

    added new baseproject with strategy pattern

---
+++ WebApp.Strategy/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProject.Controllers
+{
+    public class AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email,  string password)
+        {
+            var hasUser =  await userManager.FindByEmailAsync(email);
+            if (hasUser is null)
+            {
+                return View();
+            }
+
+            var signInResult = signInManager.PasswordSignInAsync(hasUser, password,true,false);
+
+            if (!signInResult.Result.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index),"Home");
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ WebApp.Strategy/Models/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Strategy.Models;
+
+namespace BaseProject.Models
+{
+    public class AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : IdentityDbContext<AppUser>(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
