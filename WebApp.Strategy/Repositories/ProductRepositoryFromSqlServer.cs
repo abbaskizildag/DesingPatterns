149ef0ebac695fd42c1de33a6593c64d60d82afe
Ali Kızıldağ <abbaskizildag@gmail.com>
2024-10-10 17:36:06 +0300

    added mssql and mongo repositories

---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromSqlServer.cs
@@ -0,0 +1,40 @@
+﻿using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromSqlServer(AppIdentityDbContext context) : IProductRepository
+    {
+        public async Task Delete(Product product)
+        {
+            context.Products.Remove(product);
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllByUserId(string userId)
+        {
+            return await context.Products.Where(x=>x.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> Save(Product product)
+        {
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            context.Products.Update(product);
+            await context.SaveChangesAsync();
+        }
+    }
+}
