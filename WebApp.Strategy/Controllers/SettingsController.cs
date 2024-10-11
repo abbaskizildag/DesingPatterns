b9024b9494dc4a388fda225e06e56a11ad761bda
Ali Kızıldağ <abbaskizildag@gmail.com>
2024-10-11 14:17:03 +0300

    added setting controller and models and ui

---
+++ WebApp.Strategy/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Controllers
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //cookie'de claims inde data var mı
+            Settings settings = new();
+            if (User.Claims.Where(x=>x.Type==Settings.claimDatabaseType).FirstOrDefault() != null)
+            {
+                settings.DatabaseType =(EDatabaseType) int.Parse(User.Claims.First(x=>x.Type == Settings.claimDatabaseType).Value);
+            }
+            else
+            {
+                settings.DatabaseType = settings.GetDefaultDbType;
+            }
+            return View();
+        }
+    }   
+}
